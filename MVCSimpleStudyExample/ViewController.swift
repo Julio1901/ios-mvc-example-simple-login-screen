cb21076f5894cb24faffa782f0006847dcb8e059
Julio Cesar <js.cesar42@gmail.com>
2024-04-19 21:08:43 -0300

    Initial Commit

---
+++ MVCSimpleStudyExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCSimpleStudyExample
+//
+//  Created by Julio Cesar Pereira on 19/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
